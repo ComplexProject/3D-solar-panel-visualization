320f7b64a4891f296cf433ad83f6ffb5fb10e058
JessicaDinova <dino0001@hz.nl>
2025-10-09 14:54:26 +0200

    added pull tab and its functionality with animations

--- front-end/src/AdvancedSettings.tsx
+++ front-end/src/AdvancedSettings.tsx
@@ -1,6 +1,6 @@
 function AdvancedSettings() {
     return (
-        <form className="flex flex-col gap-4 w-full h-full">
+        <form className="flex flex-col gap-[0.965rem] w-full h-full">
             <div>
                 <label htmlFor="latitude" >Latitude</label><br />
                 <input className=' px-2 py-0.5 hover:border-[#006FAA] focus:ring-1 focus:outline-none focus:ring-[#006FAA] border shadow-md border-[#808080] w-full rounded-[7px]' type="int" id="latitude"></input><br />
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,7 +5,8 @@ import './App.css'
 
 function App() {
   const [showSideMenu, setShowSideMenu] = useState(false)
-  const nodeRef = useRef<HTMLDivElement>(null)
+  const nodeRef = useRef(null)
+  const pullTabRef = useRef(null)
 
   const closeSideMenu = () => {
     setShowSideMenu(false)
@@ -13,19 +14,29 @@ function App() {
 
   return (
     <>
-      <div onClick={() => setShowSideMenu(!showSideMenu)}>
-        CLICK ME!
+
+      {/* <CSSTransition
+        in={showSideMenu}
+        timeout={100}
+        classNames="pullTab"
+        nodeRef={pullTabRef}
+      > */}
+        <div ref={pullTabRef} className={`fixed right-0 top-11 h-24 flex items-center px-5 bg-[#F8F8F8] rounded-l-4xl drop-shadow cursor-pointer transition-all duration-150
+          ${ showSideMenu ? 'lg:w-[30%] md:w-[58%] sm:w-[60%]' : 'w-[5.5rem]'}`} onClick={() => setShowSideMenu(!showSideMenu)}>
+          <svg width="42px" height="42px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M11 6L5 12L11 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M19 6L13 12L19 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
       </div>
+      {/* </CSSTransition> */}
 
       <CSSTransition
         in={showSideMenu}
         timeout={150}
-        classNames="slide-fade"
+        classNames="sideMenuSlide"
         unmountOnExit
         nodeRef={nodeRef}
-      >
+        >
         <SideMenu nodeRef={nodeRef} onClick={closeSideMenu}/>
       </CSSTransition>
+
     </>
   )
 }
