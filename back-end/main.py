75cf77225ef023981c97648fade14b52e62bcf07
Dyziex <dawidczuba1111@gmail.com>
2025-10-22 13:40:24 +0200

    resolved comments

--- back-end/main.py
+++ back-end/main.py
@@ -3,15 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:80",
-    "http://localhost:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
