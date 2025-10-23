11de85aa4d3e6ba5e7b228ac764cef18edf4574d
JessicaDinova <dino0001@hz.nl>
2025-10-23 10:57:22 +0200

    added library for react native free svgs

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,6 +4,7 @@ import { CSSTransition } from 'react-transition-group'
 import SideMenu from './SideMenu'
 import TotalEnergy from './EnergyResultsComponents/TotalEnergy'
 import UsedParameters from './EnergyResultsComponents/UsedParameters'
+import { Settings, FastArrowLeft, IconoirProvider } from 'iconoir-react';
 
 function App() {
   const [showSideMenu, setShowSideMenu] = useState(false)
@@ -17,16 +18,22 @@ function App() {
   return (
     <>
       <div className='h-screen w-full bg-red-50'>
-            <div 
-          ref={pullTabRef} 
+        <div
+          ref={pullTabRef}
           className={`absolute right-0 top-1/4 h-24 flex items-center px-5 bg-[#F8F8F8] rounded-l-4xl drop-shadow cursor-pointer transition-all duration-150
-            ${showSideMenu ? 'lg:w-[30%] md:w-[58%] sm:w-[60%]' : 'w-[5.5rem]'}`} 
+            ${showSideMenu ? 'lg:w-[30%] md:w-[58%] sm:w-[60%]' : 'w-[5.5rem]'}`}
           onClick={() => setShowSideMenu(!showSideMenu)}
         >
-          <svg width="42px" height="42px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-            <path d="M11 6L5 12L11 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-            <path d="M19 6L13 12L19 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-          </svg>
+          <IconoirProvider
+            iconProps={{
+              color: '#000000',
+              strokeWidth: 1.5,
+              width: '2.6rem',
+              height: '2.6rem',
+            }}
+          >
+            <FastArrowLeft />
+          </IconoirProvider>
         </div>
         <div className='absolute right-0 top-1/6 lg:w-1/4 md:w-1/2 sm:w-1/2 overflow-x-hidden py-1 pl-1'>
           <CSSTransition
@@ -70,12 +77,12 @@ function App() {
         <div className='bg-white w-full h-full flex flex-col p-11 gap-10 drop-shadow rounded-2xl'>
           <h1 className=' text-2xl font-bold'>Used parameters</h1>
           <div className='grid grid-cols-3 gap-16'>
-            <UsedParameters title='Latitude' parameter='51.498'/>
-            <UsedParameters title='Longitude' parameter='3.618'/>
-            <UsedParameters title='Slope increment' parameter='2° increment'/>
-            <UsedParameters title='Azimuth increment' parameter='5° increment'/>
-            <UsedParameters title='PV max power' parameter='10 kWp'/>
-            <UsedParameters title='PV longevity' parameter='15 years'/>
+            <UsedParameters title='Latitude' parameter='51.498' />
+            <UsedParameters title='Longitude' parameter='3.618' />
+            <UsedParameters title='Slope increment' parameter='2° increment' />
+            <UsedParameters title='Azimuth increment' parameter='5° increment' />
+            <UsedParameters title='PV max power' parameter='10 kWp' />
+            <UsedParameters title='PV longevity' parameter='15 years' />
           </div>
         </div>
       </div>
--- front-end/src/DropZone.tsx
+++ front-end/src/DropZone.tsx
@@ -1,6 +1,7 @@
-import {useMemo} from 'react';
+import { useMemo } from 'react';
 import { useState } from 'react';
-import {useDropzone} from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
+import { IconoirProvider, CloudUpload, SmallLampAlt } from 'iconoir-react';
 
 const focusedStyle: React.CSSProperties = {
   borderColor: '#2196f3'
@@ -24,7 +25,7 @@ function StyledDropzone() {
     isDragAccept,
     isDragReject
   } = useDropzone({
-    accept: {'image/*': []},
+    accept: { 'image/*': [] },
     maxFiles: 1,
     onDrop: (acceptedFiles) => {
       if (acceptedFiles.length > 0) {
@@ -45,30 +46,41 @@ function StyledDropzone() {
     isDragReject
   ]);
 
-  const displayFileName = (fileName: string) => {
-    if (fileName.length > 26) {
-      return fileName.substring(0, 23) + '...';
-    }
-    return fileName;
-  };
+  // const displayFileName = (fileName: string) => {
+  //   if (fileName.length > 26) {
+  //     return fileName.substring(0, 23) + '...';
+  //   }
+  //   return fileName;
+  // };
 
   return (
     <div className="container">
-      <div {...getRootProps({style})} className='flex flex-col justify-center items-center p-5 border-2 rounded-xl hover:cursor-pointer hover:border-[#006FAA] border-[#808080] border-dashed'>
+      <div {...getRootProps({ style })} className='flex flex-col justify-center items-center p-5 border-2 rounded-xl hover:cursor-pointer hover:border-[#006FAA] border-[#808080] border-dashed'>
         <input {...getInputProps()} />
-        {!file ? ( <>
-        <svg width="24px" height="24px" strokeWidth={1.5} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 22V13M12 13L15.5 16.5M12 13L8.5 16.5" stroke="#000000" strokeWidth={1.5} strokeLinecap='round' strokeLinejoin='round'></path><path d="M20 17.6073C21.4937 17.0221 23 15.6889 23 13C23 9 19.6667 8 18 8C18 6 18 2 12 2C6 2 6 6 6 8C4.33333 8 1 9 1 13C1 15.6889 2.50628 17.0221 4 17.6073" stroke="#000000" strokeWidth={1.5} strokeLinecap='round' strokeLinejoin='round'></path></svg>
-            <p>Drag & Drop your file</p> </>)
-          :( <>
-          <div className='flex flex-row items-center'>
-            <svg width="30px" height="30px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6.87172 3.42759L4.23172 12.2276C4.11623 12.6126 4.4045 13 4.80642 13L19.1936 13C19.5955 13 19.8838 12.6126 19.7683 12.2276L17.1283 3.42759C17.0521 3.1738 16.8185 3 16.5536 3L7.44642 3C7.18145 3 6.94786 3.1738 6.87172 3.42759Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8 15L8 13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8 21H16M12 15L12 21" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-            <p>{displayFileName(file.name)}</p>
-          </div> </>)
-        }
+        <IconoirProvider
+          iconProps={{
+            color: '#000000',
+            strokeWidth: 1.5,
+            width: '24px',
+            height: '24px',
+          }}
+        >
+          {!file ? (<>
+              <CloudUpload />
+              <p>Drag & Drop your file</p> 
+            </>)
+            : (<>
+              <div className='flex flex-row items-center w-full justify-center'>
+                <SmallLampAlt />
+                <p className='truncate'>{file.name}</p>
+              </div> 
+            </>)
+          }
+        </IconoirProvider>
         <p>Or</p>
-      <button className=' rounded-[10px] bg-[#D9D9D9] h-8 px-3.5 hover:cursor-pointer hover:bg-[#a8a8a8]'>
-            Browse files
-      </button>
+        <button className=' rounded-[10px] bg-[#D9D9D9] h-8 px-3.5 hover:cursor-pointer hover:bg-[#a8a8a8]'>
+          {!file ? "Browse files" : "Choose a different file"}
+        </button>
       </div>
     </div>
   );
