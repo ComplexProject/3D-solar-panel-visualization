07260acde385389316d96087cdc32b03c58472b0
Dyziex <dawidczuba1111@gmail.com>
2025-10-23 14:20:49 +0200

    api

--- front-end/dummyDocker/main.py
+++ front-end/dummyDocker/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/getDummy")
 def getDummy():
     return {"totalEnergy": 3000,
@@ -20,4 +28,4 @@ def getDummy():
 
 if __name__ == "__main__":
 	import uvicorn
-	uvicorn.run(app, host="0.0.0.0", port=8510, reload=True)
+	uvicorn.run(app, host="127.0.0.1", port=8510, reload=True)
