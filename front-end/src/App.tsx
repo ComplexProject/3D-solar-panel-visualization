5b149f5da6eee261cb36244eaba1610cedf7ef70
Kristers Davis Gruzins <gruz0001@hz.nl>
2025-11-04 23:42:04 +0100

    Updated the CSS so it works now

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,7 +4,9 @@ import { CSSTransition } from 'react-transition-group'
 import SideMenu from './SideMenu'
 import TotalEnergy from './EnergyResultsComponents/TotalEnergy'
 import UsedParameters from './EnergyResultsComponents/UsedParameters'
-import {getDummyData} from './api'
+import ModelViewer from './ModelImportComponent/ModelViewer'
+import BuildingWithSolarPanels from './ModelImportComponent/BuildingWithSolarPanels'
+import { getDummyData } from './api'
 
 function App() {
   const [showSideMenu, setShowSideMenu] = useState(false)
@@ -33,30 +35,37 @@ function App() {
 
   return (
     <>
-      <div className='h-screen w-full bg-red-50'>
-            <div 
-          ref={pullTabRef} 
-          className={`absolute right-0 top-1/4 h-24 flex items-center px-5 bg-[#F8F8F8] rounded-l-4xl drop-shadow cursor-pointer transition-all duration-150
-            ${showSideMenu ? 'lg:w-[30%] md:w-[58%] sm:w-[60%]' : 'w-[5.5rem]'}`} 
-          onClick={() => setShowSideMenu(!showSideMenu)}
-        >
-          <svg width="42px" height="42px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-            <path d="M11 6L5 12L11 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-            <path d="M19 6L13 12L19 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-          </svg>
-        </div>
-        <div className='absolute right-0 top-1/6 lg:w-1/4 md:w-1/2 sm:w-1/2 overflow-x-hidden py-1 pl-1'>
-          <CSSTransition
-            in={showSideMenu}
-            timeout={150}
-            classNames="sideMenuSlide"
-            unmountOnExit
-            nodeRef={nodeRef}
+      <div className="h-screen w-full bg-red-50 relative">
+        <div className="relative h-full w-full">
+          <ModelViewer>
+            <BuildingWithSolarPanels />
+          </ModelViewer>
+          <div
+            ref={pullTabRef}
+            className={`absolute right-0 top-1/4 h-24 flex items-center px-5 bg-[#F8F8F8] rounded-l-4xl drop-shadow cursor-pointer transition-all duration-150
+        ${showSideMenu ? 'lg:w-[30%] md:w-[58%] sm:w-[60%]' : 'w-[5.5rem]'}`}
+            onClick={() => setShowSideMenu(!showSideMenu)}
+            style={{ zIndex: 20 }}
           >
-            <SideMenu nodeRef={nodeRef} onClick={closeSideMenu} />
-          </CSSTransition>
+            <svg width="42px" height="42px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+              <path d="M11 6L5 12L11 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+              <path d="M19 6L13 12L19 18" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+            </svg>
+          </div>
+          <div className={`absolute right-0 top-1/6 transition-all duration-150 ${showSideMenu ? 'lg:w-1/4 md:w-1/2 sm:w-1/2' : 'w-0'} overflow-x-hidden py-1 pl-1`} style={{ zIndex: 30 }}>
+            <CSSTransition
+              in={showSideMenu}
+              timeout={150}
+              classNames="sideMenuSlide"
+              unmountOnExit
+              nodeRef={nodeRef}
+            >
+              <SideMenu nodeRef={nodeRef} onClick={closeSideMenu} />
+            </CSSTransition>
+          </div>
         </div>
       </div>
+
       <div className='px-12 py-16 flex flex-col h-full w-full gap-11 bg-[#F8F8F8]'>
         <h1 className=' font-bold text-5xl'>Results</h1>
         <div className='w-full h-full flex flex-row gap-16'>
@@ -101,12 +110,12 @@ function App() {
         <div className='bg-white w-full h-full flex flex-col p-11 gap-10 drop-shadow rounded-2xl'>
           <h1 className=' text-2xl font-bold'>Used parameters</h1>
           <div className='grid grid-cols-3 gap-16'>
-            <UsedParameters title='Latitude' parameter='51.498'/>
-            <UsedParameters title='Longitude' parameter='3.618'/>
-            <UsedParameters title='Slope increment' parameter='2° increment'/>
-            <UsedParameters title='Azimuth increment' parameter='5° increment'/>
-            <UsedParameters title='PV max power' parameter='10 kWp'/>
-            <UsedParameters title='PV longevity' parameter='15 years'/>
+            <UsedParameters title='Latitude' parameter='51.498' />
+            <UsedParameters title='Longitude' parameter='3.618' />
+            <UsedParameters title='Slope increment' parameter='2° increment' />
+            <UsedParameters title='Azimuth increment' parameter='5° increment' />
+            <UsedParameters title='PV max power' parameter='10 kWp' />
+            <UsedParameters title='PV longevity' parameter='15 years' />
           </div>
         </div>
       </div>
