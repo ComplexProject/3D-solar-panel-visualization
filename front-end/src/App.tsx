4f25097195eac1483bb55638c1867e7044bd4f25
JessicaDinova <dino0001@hz.nl>
2025-11-18 13:30:13 +0100

    fixed the result css where the div was not fitting properly and changed the total enrgy component inmplementation so that the titles stay in place

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -83,7 +83,7 @@ function App() {
           </div>
         </div>
           <div className='w-full h-full flex flex-row gap-10'>
-            <div className='bg-white p-10 gap-10 drop-shadow rounded-2xl w-2/3 h-fit flex flex-col min-w-0'>
+            <div className='bg-white p-10 gap-10 drop-shadow rounded-2xl w-2/3 flex flex-col min-w-0'>
               <h1 className=' text-2xl font-bold'>Optimal solar placement</h1>
               <div className='grid grid-rows-2 grid-flow-col gap-10 w-full overflow-x-auto overflow-y-visible'>
                 {dummyData ? (
@@ -95,7 +95,7 @@ function App() {
                   )}
               </div>
             </div>
-            <div className='bg-white drop-shadow rounded-2xl flex flex-col w-1/3 min-w-0 '>
+            <div className='bg-white drop-shadow rounded-2xl flex flex-col w-1/3'>
               <h1 className='font-bold text-2xl p-10'>Produced solar energy</h1>
               <hr className='border-1'/>
               <div className='px-10 py-1 text-2xl divide-y divide-black max-h-[300px] overflow-auto'>
@@ -109,7 +109,14 @@ function App() {
             </div>
           </div>
         <div className='w-full h-full flex flex-row gap-10'>
-          {dummyData ? (
+          <div className='flex w-2/3 gap-10'>
+            <TotalEnergy title='Total energy demand' results={dummyData ? `${dummyData.totalEnergy} kWp` : 'Loading...'}/>
+            <TotalEnergy title='Energy from the grid' results={dummyData ? `${dummyData.energyFromGrid} kWp` : 'Loading...'}/>
+          </div>
+          <div className='w-1/3'>
+            <TotalEnergy title='PV Energy production' results={dummyData ? `${dummyData.pvProduction} kWp` : 'Loading...'}/>
+          </div>
+          {/* {dummyData ? (
             <>
               <div className='flex w-2/3 gap-10'>
                 <TotalEnergy title='Total energy demand' results={dummyData.totalEnergy} />
@@ -121,7 +128,7 @@ function App() {
             </>
             ) : (
             <p>Loading data...</p>
-          )}
+          )} */}
         </div>
         <div className='bg-white w-full h-full flex flex-col p-11 gap-10 drop-shadow rounded-2xl'>
           <h1 className=' text-2xl font-bold'>Used parameters</h1>
