4c954dd435aa001000ae635adabf55ba5385874f
JessicaDinova <dino0001@hz.nl>
2025-11-18 12:01:19 +0100

    added * to important fields, added placeholders, added a tooltip to let the users know what headers need to be included in the demand profile before submiting

--- front-end/src/formComponents/ParametersForm.tsx
+++ front-end/src/formComponents/ParametersForm.tsx
@@ -2,25 +2,26 @@ import StyledDropzone from './DropZone'
 import { useForm } from 'react-hook-form'
 import { useState, useEffect } from 'react';
 import { GetGeocodingData } from '../utils/apiTesting';
+import { IconoirProvider, InfoCircle, Triangle } from 'iconoir-react';
 
 const inputClass = 'px-2 py-0.5 hover:border-[#006FAA] focus:ring-1 focus:outline-none focus:ring-[#006FAA] border shadow-md border-[#808080] w-full rounded-[7px]'
 
 interface FormData {
-  city: string;
-  power: number;
+    city: string;
+    power: number;
 }
 
 function ParameterForm() {
     const { register, setValue } = useForm<FormData>();
-    
+
     const [formData, setFormData] = useState<FormData>(() => {
         const savedCity = localStorage.getItem("city");
         const savedPower = localStorage.getItem("power");
-        
+
         return {
-        city: savedCity ? JSON.parse(savedCity) : undefined,
-        power: savedPower ? JSON.parse(savedPower) : undefined,
-    };
+            city: savedCity ? JSON.parse(savedCity) : undefined,
+            power: savedPower ? JSON.parse(savedPower) : undefined,
+        };
     });
 
     useEffect(() => {
@@ -49,18 +50,57 @@ function ParameterForm() {
         setFormData(prev => ({ ...prev, power: powerNumber }));
     }
 
-    return(
-        <form id='parameter-form'  className='flex flex-col gap-4 w-full h-full'>
+    return (
+        <form id='parameter-form' className='flex flex-col gap-4 w-full h-full'>
             <div>
-                <label htmlFor="city">City</label><br />
-                <input className={inputClass} type="text" {...register("city", { onBlur: handleCityBlur })} id="city"/><br />
+                <div className='flex flex-row gap-1'>
+                    <label htmlFor="city">City</label>
+                    <p className='text-red-500'>*</p>
+                </div>
+                <input className={inputClass} type="text" placeholder='Middelburg' {...register("city", { onBlur: handleCityBlur })} id="city" /><br />
             </div>
             <div>
-                <label htmlFor="power">Maximum PV power (kWp)</label><br />
-                <input className={inputClass} type="number" step={0.1} {...register("power", { onChange: handlePowerChange })} id="power"/><br />
+                <div className='flex flex-row gap-1'>
+                    <label htmlFor="power">Maximum PV power (kWp)</label><br />
+                    <p className='text-red-500'>*</p>
+                </div>
+                <input className={inputClass} type="number" step={0.1} placeholder='15' {...register("power", { onChange: handlePowerChange })} id="power" /><br />
             </div>
             <div>
-                <label htmlFor="file">Upload demand profile</label><br />
+                <div className='flex flex-row justify-between'>
+                    <div className='flex flex-row gap-1'>
+                        <label htmlFor="file">Upload demand profile</label>
+                        <p className='text-red-500'>*</p>
+                    </div>
+                    <div className='cursor-pointer flex items-center group '>
+                        <div className='absolute z-50 pl-5 pr-4 hidden group-hover:flex right-14 justify-center items-center drop-shadow-xl'>
+                            <div className='bg-gray-300 p-1 rounded-md px-1.5'>
+                                <p>Headers required: bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla, bla</p>
+                            </div>
+                            <IconoirProvider
+                                iconProps={{
+                                    className:'rotate-90 absolute right-2',
+                                    fill:'#d1d5dc',
+                                    color: '#d1d5dc',
+                                    strokeWidth: 1.5,
+                                    width: '0.9rem',
+                                    height: '0.9rem',
+                                }}>
+                                <Triangle />
+                            </IconoirProvider>
+                        </div>
+                        <IconoirProvider
+                            iconProps={{
+                                className: 'hover:scale-110',
+                                color: 'black',
+                                strokeWidth: 1.5,
+                                width: '1.1rem',
+                                height: '1.1rem',
+                            }}>
+                            <InfoCircle />
+                        </IconoirProvider>
+                    </div>
+                </div>
                 <p className='text-[12px] text-[#717171]'>Allowed files: CSV, JSON</p>
                 <StyledDropzone />
             </div>
