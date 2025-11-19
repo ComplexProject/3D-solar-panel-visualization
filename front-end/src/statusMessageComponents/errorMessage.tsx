f1d56196804a7a09757b2046009c93b03da1265b
rgl-lopes <leit0005@hz.nl>
2025-11-19 14:53:57 +0100

    Added icon to errorMessage, CSS improvement

--- front-end/src/statusMessageComponents/errorMessage.tsx
+++ front-end/src/statusMessageComponents/errorMessage.tsx
@@ -1,8 +1,24 @@
+import { WarningTriangleSolid, IconoirProvider } from 'iconoir-react';
+
+
 function errorMessage() {
     return (
         <>
-            <div className="bg-[#FFDEDE] border-1 border-[#FF3131] rounded-2xl p-6">
-                <h1 className="text-4xl font-semibold mb-2">Error</h1>
+            <div className="bg-[#FFDEDE] border border-[#FF3131] rounded-2xl p-6">
+                <h1 className="text-4xl font-semibold mb-2 flex">
+                        <IconoirProvider
+                        iconProps={{
+                        color: '#f21b1b',
+                        stroke: '#ffffff',
+                        strokeWidth: 0.5,
+                        width: '2.6rem',
+                        height: '2.6rem',
+                        }}
+                    >
+                        <WarningTriangleSolid />
+                    </IconoirProvider>
+                    <p className='ml-2'>Error</p>
+                </h1>
                 <p className="text-lg">Data retrieval has failed</p>
             </div>
         </>
--- front-end/src/statusMessageComponents/loadingMessage.tsx
+++ front-end/src/statusMessageComponents/loadingMessage.tsx
@@ -1,7 +1,7 @@
 function loadingMessage() {
     return (
         <>
-            <div className="bg-[#FDFFDA] border-1 border-[#FFE3AE] rounded-2xl p-6">
+            <div className="bg-[#FDFFDA] border border-[#FFE3AE] rounded-2xl p-6">
                 <h1 className="text-4xl font-semibold mb-2">Loading</h1>
                 <p className="text-lg">Please wait until data retrieval has finished</p>
             </div>
