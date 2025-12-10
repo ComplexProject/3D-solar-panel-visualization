3505e7f68826d5afd734511bce9d30fa4544b138
rgl-lopes <leit0005@hz.nl>
2025-12-10 13:58:25 +0100

    Changed message styling

--- front-end/src/statusMessageComponents/loadingMessage3DViewer.tsx
+++ front-end/src/statusMessageComponents/loadingMessage3DViewer.tsx
@@ -3,9 +3,9 @@ import Warning from '../assets/warning.png';
 function loadingMessageModelViewer() {
     return (
         <>
-            <div className="m-20 absolute z-50 bg-[#FDFFDA] border border-[#FFE3AE] rounded-2xl p-3 w-xs">
+            <div className="m-20 absolute z-50 bg-[#FDFFDA] border border-[#FFE3AE] rounded-2xl p-3 w-xs pointer-events-none">
                 <div className='flex items-start gap-2'>
-                    <img src={Warning} className='mt-1 h-[2em] w-[2.5em]'></img>
+                    <img src={Warning} className='mt-1 h-[2em] w-[3em]'></img>
                     <p className="text-xl font-semibold">Currently, there are no solar panels in the 3D viewer</p> 
                 </div>                
             </div>
